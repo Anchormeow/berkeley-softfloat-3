e6a36ec4eff52839b037d86d88712d55055f4162
Anchormeow <chy031608@163.com>
2024-12-26 16:12:22 +0800

    2024-12-26 fp16 add test

--- csrc/add.c
+++ csrc/add.c
@@ -2,30 +2,53 @@
 #include <string.h>
 #include "softfloat.h"
 
+void calculate_sum(float input[], int size) {
+    // FP16 sum init
+    float16_t sum_f16 = {0};
+
+    printf("FP16 Expression: ");
+    for (int i = 0; i < size; ++i) {
+        float32_t num_f32;
+        memcpy(&num_f32, &input[i], sizeof(float)); // 将输入转换为float32_t
+        float16_t num_f16 = f32_to_f16(num_f32);   // 转换为float16_t
+        sum_f16 = f16_add(sum_f16, num_f16);       // 累加
+
+        // 打印算式
+        if (i > 0) {
+            printf(" + ");
+        }
+        printf("%.8f (%x)", input[i], num_f16.v);
+
+    }
+
+    // 转换累加结果为float32并打印
+    float result;
+    float32_t result_f32 = f16_to_f32(sum_f16);
+    printf(" = %x in fp16", sum_f16.v);
+    memcpy(&result, &result_f32, sizeof(float));
+    printf(" = %.8f in fp32\n", result);
+
+    // FP32 sum init
+    float sum_f32 = 0.0f;
+
+    printf("FP32 Expression: ");
+    for (int i = 0; i < size; ++i) {
+        sum_f32 = sum_f32 + input[i];       // 累加
+
+        // 打印算式
+        if (i > 0) {
+            printf(" + ");
+        }
+        printf("%.8f", input[i]);
+    }
+    printf(" = %.8f\n", sum_f32);
+}
+
 int main(void) {
-    float a = 0.1;
-    float32_t ai;
-    memcpy(&ai, &a, sizeof(a));
-    printf("ai is: %x\n", ai.v);
-    float16_t af = f32_to_f16(ai);
-    printf("af is: %u %x\n", af.v, af.v);
+    float input[] = {0.1f, 0.2f, 0.3f, 0.4f};
+    int size = sizeof(input) / sizeof(input[0]);
 
-    float b = 0.2;
-    float32_t bi;
-    memcpy(&bi, &b, sizeof(b));
-    float16_t bf = f32_to_f16(bi);
-    printf("bf is: %u %x\n", bf.v, bf.v);
+    calculate_sum(input, size);
 
-    float16_t cf = f16_mul(af, bf);
-    float16_t df = f16_add(af, bf);
-
-    float c, d;
-    float32_t ci = f16_to_f32(cf);
-    memcpy(&c, &ci, sizeof(c));
-    printf("c is: %.8f\n",c);
-    float32_t di = f16_to_f32(df);
-    memcpy(&d, &di, sizeof(d));
-    printf("d is: %.8f\n",d);
-    
     return 0;
 }
--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ WAIT_TIME=2  # 设置等待时间为 2 秒
 
 cd ./build/Linux-x86_64-GCC
 
-
+echo "cheng:"
 ./cheng
+echo
+echo "add:"
 ./add
